d8b2508d4e2894273dfd03f28bfe8d46cfbe01ad
Ludwig Prager <ludwig.prager@celp.de>
2023-08-06 02:47:16 +0200

    using ggml-alpaca-7b-q4 as default

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM python:3.11-slim
 
-
 RUN apt-get update && apt-get install -y \
     build-essential \
     libclblast-dev \
@@ -15,8 +14,4 @@ WORKDIR /work/kopboldcpp
 RUN pip install --no-cache-dir --trusted-host pypi.python.org -r requirements.txt
 RUN make LLAMA_OPENBLAS=1 LLAMA_CLBLAST=1
 
-# Make port 80 available to the world outside this container
-EXPOSE 80
-
-# Use koboldcpp.py as the entrypoint when the container launches
 ENTRYPOINT ["python", "koboldcpp.py"]
--- start.sh
+++ start.sh
@@ -6,11 +6,16 @@ cd ${BASEDIR}
 
 URL=https://huggingface.co/TehVenom/Metharme-7b-4bit-Q4_1-GGML/resolve/main/Metharme-7b-4bit-Q4_1-GGML-V2.bin
 URL=https://huggingface.co/eachadea/legacy-ggml-vicuna-7b-4bit/resolve/main/ggml-vicuna-7b-4bit-rev1.bin
+URL=https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGML/resolve/main/llama-2-70b-chat.ggmlv3.q2_K.bin
+URL=https://huggingface.co/TheBloke/LongChat-7B-GGML/resolve/main/longchat-7b-16k.ggmlv3.q2_K.bin
+URL=https://huggingface.co/localmodels/LLaMA-7B-ggml/resolve/main/llama-7b.ggmlv3.q2_K.bin
+URL=https://huggingface.co/Sosaka/Alpaca-native-4bit-ggml/resolve/main/ggml-alpaca-7b-q4.bin
 
 export MODEL=$(basename ${URL} )
 
 mkdir -p models
 
+# using docker to download because I promised that only docker is required
 if [[ ! -f models/${MODEL} ]]; then
   docker run -it --rm \
     -v $(pwd)/models:/models/ \
